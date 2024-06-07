aa07e79068ce2a8b0d5419cf8d452d65ed5c3868
anujkp-97 <anujpal9770@gmail.com>
2024-06-07 22:52:34 +0530

    day-04

---
+++ Day_04/list.py
@@ -0,0 +1,13 @@
+list_state = ["Andhra Pradesh","Arunachal Pradesh ","Assam","Bihar",
+              "Chhattisgarh","Goa","Gujarat","Haryana","Himachal Pradesh",
+              "Jammu and Kashmir","Jharkhand","Karnataka","Kerala","Madhya Pradesh",
+              "Maharashtra","Manipur","Meghalaya","Mizoram","Nagaland","Odisha",
+              "Punjab","Rajasthan","Sikkim","Tamil Nadu","Telangana","Tripura",
+              "Uttar Pradesh","Uttarakhand","West Bengal","Andaman and Nicobar Islands",
+              "Chandigarh","Dadra and Nagar Haveli","Daman and Diu","Lakshadweep",
+              "National Capital Territory of Delhi","Puducherry"]
+
+# print(list_state[0])
+
+for state in list_state:
+    print(state)
