09526b88c3816d0705321cc605d88964985da8a5
anujkp-97 <anujpal9770@gmail.com>
2024-06-08 18:13:32 +0530

    Day-06

---
+++ Day_06/functionn.py
@@ -0,0 +1,18 @@
+
+
+# built-in function
+
+print("Hello")
+num_char = len("Hello")
+print(num_char)
+
+
+def my_function():
+    print("Hello from my function")
+    print("Hello this is the second line of the funciton!")
+
+print("Is this out from the function!")
+
+
+
+my_function()
